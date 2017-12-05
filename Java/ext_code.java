2ae735d2aa84a77dda2b99a43ec3c071e0ee05ad
evilate <allan@allan.eu>
2017-12-05 05:10:09 +0100

    Update ext_code.java

--- Java/ext_code.java
+++ Java/ext_code.java
@@ -9,3 +9,16 @@
                                 Toast.LENGTH_LONG).show();
                 }
             }, 2000);
+
+final Handler handler = new Handler();
+b1.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+     handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+  Toast.makeText(MainActivity.this,"Welcome to my first project in android",Toast.LENGTH_LONG).show();
+
+}, 20000); //in milliseconds
+
+}); 
